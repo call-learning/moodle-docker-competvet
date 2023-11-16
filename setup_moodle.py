80a1f31d20014661dd4a278fb29ce2fd5afe7cd5
Laurent David <lmedavid@gmail.com>
2023-11-16 08:21:28 +0100

    Intial commit

---
+++ setup_moodle.py
@@ -0,0 +1,56 @@
+from git import Repo
+import shutil
+import os
+import setup_utils
+# If directory moodle-src does not exist, create it.
+# Get current directory in CURRENT_DIR variable
+
+CURRENT_DIR = os.getcwd()
+MOODLE_SRC_DIR = CURRENT_DIR + '/moodle-src'
+if not os.path.isdir(MOODLE_SRC_DIR):
+    os.mkdir(MOODLE_SRC_DIR)
+# Check if moodle-src is empty and has a git repository cloned already
+if os.path.isdir(MOODLE_SRC_DIR + '/.git'):
+    print('moodle-src already has a git repository. Assuming this is the '
+          'moodle repository.')
+else:
+    # Clone the moodle repository into moodle-src
+    print('Cloning Moodle repository in moodle-src.')
+    Repo.clone_from('http://github.com/moodle/moodle.git', 'moodle-src')
+# Change directory to moodle-src
+os.chdir(MOODLE_SRC_DIR)
+# Checkout the latest stable version
+Repo().git.checkout('MOODLE_401_STABLE')
+# Then add two modules
+print('Cloning CompetVetEval Module plugin.')
+setup_utils.clone_or_update_modules(MOODLE_SRC_DIR, '/mod/','competvet','https://github.com/call-learning/moodle-mod_competvet.git')
+
+print('Cloning CompetVetEval Local plugin.')
+setup_utils.clone_or_update_modules(MOODLE_SRC_DIR, '/local/','competvet','https://github.com/call-learning/moodle-local_competvet.git')
+
+setup_utils.setup_env(MOODLE_SRC_DIR)
+
+# copy the moodle-docker/config.docker-template.php to moodle-src/config.php
+if not os.path.isfile(MOODLE_SRC_DIR + '/config.php'):
+    shutil.copyfile(CURRENT_DIR + '/moodle-docker/config.docker-template.php', MOODLE_SRC_DIR + '/config.php')
+
+# depending if on windows or linux, run the appropriate docker-compose command to start the containers
+print('Bringing container up.')
+os.chdir(CURRENT_DIR + '/moodle-docker')
+DOCKER_COMPOSE_CMD = setup_utils.get_docker_compose_command()
+os.system(DOCKER_COMPOSE_CMD + ' up -d')
+setup_utils.wait_for_db()
+# # Install Moodle
+print('Installing Moodle.')
+setup_utils.run_moodle_cli('admin/cli/install_database.php',
+               '--agree-license',
+               '--fullname="CompetVetMOODLE"',
+               '--shortname="CompetVetMOODLE"',
+               '--adminpass="test"',
+               '--adminemail="admin@example.com"'
+               )
+
+print('Finished. You can now send your command with '
+      '"bin/moodle-docker-compose.cmd" as per documented on '
+      'https://github.com/moodlehq/moodle-docker/blob/master/README.md. and '
+      'destroy the containers with "bin/moodle-docker-compose.cmd down"')
---
+++ setup_utils.py
@@ -0,0 +1,40 @@
+# Create a function setup_env
+import os
+from git import Repo
+
+def setup_env(moodle_src_dir):
+    os.environ['MOODLE_DOCKER_WWWROOT'] = moodle_src_dir
+    os.environ['MOODLE_DOCKER_DB'] = 'mysql'
+    os.environ['MOODLE_DOCKER_WEB_PORT'] = '8000'
+
+def get_docker_compose_command():
+    if os.name == 'nt':
+        return 'bin/moodle-docker-compose.cmd'
+    else:
+        return 'bin/moodle-docker-compose'
+
+def clone_or_update_modules(MOODLE_SRC_DIR, MODULE_INSTALL_DIR,MODULE_NAME,GIT_REPO_URL):
+    os.chdir(MOODLE_SRC_DIR + '/mod')
+    MODULE_FULLDIR = MOODLE_SRC_DIR + '/'+ MODULE_INSTALL_DIR + '/' + MODULE_NAME
+    if not os.path.isdir(MODULE_FULLDIR):
+        Repo.clone_from(
+        GIT_REPO_URL,
+        MODULE_NAME)
+    else:
+        print('Module plugin already cloned. Assuming this is the '
+          'plugin repository, updating it.')
+        Repo(MODULE_FULLDIR).git.pull()
+
+def wait_for_db():
+    DOCKER_COMPOSE_CMD = get_docker_compose_command()
+    print("Waiting for database to be ready...")
+    result = os.popen(DOCKER_COMPOSE_CMD + ' logs db').read()
+    # Wait until result contains 'DATABASE IS READY TO USE!'
+    while '/usr/sbin/mysqld: ready for connections' not in result:
+        result = os.popen(DOCKER_COMPOSE_CMD + ' logs db').read()
+
+def run_moodle_cli(CLI_COMMAND, *ARGS):
+    DOCKER_COMPOSE_CMD = get_docker_compose_command()
+    # Call a external command and send argments separated by space
+    os.system(DOCKER_COMPOSE_CMD + ' exec -T webserver php ' + CLI_COMMAND + ' ' + ' '.join(ARGS))
+
